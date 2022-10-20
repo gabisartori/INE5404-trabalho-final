22cb9c0f29fc1d929a13a3c59798e8da991ffd8b
clara2023 <105126932+clara2023@users.noreply.github.com>
2022-10-20 11:11:09 -0300

    Correção loop de repetição

--- tela_login.py
+++ tela_login.py
@@ -1,71 +1,59 @@
 from tkinter import *
 import tkinter as tk
-import pygame
 
-pygame.init()
+class Application:
 
-while True:
-    
+    def __init__ (self, master = None):
+        
+        self.fonte = ("Verdana", "8")
+        self.label = tk.Label (text = "FAZER LOGIN", font = ("Arial", 25, "italic", "bold"), foreground = "black", background = "#62c9de", width = 50, height =8)
+        self.label.pack()
 
-    class Application:
+        self.container3 = Frame(master)
+        self.container3["padx"] = 180
+        self.container3["pady"] = 60
+        self.container3.pack()
 
-        def __init__ (self, master = None):
-            
-            self.fonte = ("Verdana", "8")
-            self.label = tk.Label (text = "FAZER LOGIN", font = ("Arial", 25, "italic", "bold"), foreground = "black", background = "#62c9de", width = 50, height =8)
-            self.label.pack()
+        self.container6 = Frame(master)
+        self.container6["padx"] = 20
+        self.container6["pady"] = 5
+        self.container6.pack()
 
-            self.container3 = Frame(master)
-            self.container3["padx"] = 180
-            self.container3.pack()
+        self.container2 = Frame(master)
+        self.container2["padx"] = 20
+        self.container2.pack()
+        
+        self.container4 = Frame(master)
+        self.container4["padx"] = 20
+        self.container4["pady"] = 50
+        self.container4.pack()
 
-            self.container6 = Frame(master)
-            self.container6["padx"] = 20
-            self.container6["pady"] = 5
-            self.container6.pack()
+        self.container1 = Frame(master)
+        self.container1["pady"] = 10
+        self.container1.pack()       
 
-            self.container2 = Frame(master)
-            self.container2["padx"] = 20
-            self.container2.pack()
-            
-            self.container4 = Frame(master)
-            self.container4["padx"] = 20
-            self.container4["pady"] = 50
-            self.container4.pack()
+        self.lblnome = tk.Label (text = "Email:", font=("Verdana", 18, "italic"), foreground = "black", background = "#62c9de", width = 80)
+        self.lblnome.pack(side=LEFT)
+        #adicionar informacoes
 
-            self.container1 = Frame(master)
-            self.container1["pady"] = 10
-            self.container1.pack()       
-
-
-            #adicionar informacoes
-
-            self.nome = Entry(self.container2)
-            self.nome["width"] = 25
-            self.nome["font"] = "Verdana", 18, "italic"
-            self.nome.pack(side=LEFT)
+        self.nome = Entry(self.container2)
+        self.nome["width"] = 25
+        self.nome["font"] = "Verdana", 18, "italic"
+        self.nome.pack(side=LEFT)
 
 
-            self.nome = Entry(self.container4)
-            self.nome["width"] = 25
-            self.nome["font"] = "Verdana", 18, "italic"
-            self.nome.pack(side= LEFT)
-            
+        self.nome = Entry(self.container4)
+        self.nome["width"] = 25
+        self.nome["font"] = "Verdana", 18, "italic"
+        self.nome.pack(side= LEFT)
+        
 
-    root = Tk()
-
-
-    root.geometry('900x700')
-    root.configure(background="#62c9de")
-    Application(root)
-
-    root.mainloop()
+root = Tk()
 
 
-'''
-            self.lblnome = tk.Label (text = "Email:", font=("Verdana", 18, "italic"), foreground = "black", background = "#62c9de", width = 30, height =10)
-            self.lblnome.pack(side=LEFT)
-            self.lblnome = tk.Label (text = "Senha:", font=("Verdana", 18, "italic"), foreground = "black", background = "#62c9de", width = 30, height =40) 
-            self.lblnome.pack(side=LEFT)
-            
-'''
+root.geometry('900x700')
+root.configure(background="#62c9de")
+Application(root)
+
+root.mainloop()
+

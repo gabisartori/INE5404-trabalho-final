975bdf2149b98467ab272cbd9198c1359fb62540
clara2023 <105126932+clara2023@users.noreply.github.com>
2022-10-20 10:52:27 -0300

    Entrada de informações

--- tela_login.py
+++ tela_login.py
@@ -1,71 +1,71 @@
 from tkinter import *
-
 import tkinter as tk
+import pygame
 
- 
+pygame.init()
 
-class Application:
+while True:
+    
 
-    def __init__ (self, master = None):
+    class Application:
 
- 
+        def __init__ (self, master = None):
+            
+            self.fonte = ("Verdana", "8")
+            self.label = tk.Label (text = "FAZER LOGIN", font = ("Arial", 25, "italic", "bold"), foreground = "black", background = "#62c9de", width = 50, height =8)
+            self.label.pack()
 
-        self.fonte = ("Verdana", "8")
+            self.container3 = Frame(master)
+            self.container3["padx"] = 180
+            self.container3.pack()
 
- 
+            self.container6 = Frame(master)
+            self.container6["padx"] = 20
+            self.container6["pady"] = 5
+            self.container6.pack()
 
- 
+            self.container2 = Frame(master)
+            self.container2["padx"] = 20
+            self.container2.pack()
+            
+            self.container4 = Frame(master)
+            self.container4["padx"] = 20
+            self.container4["pady"] = 50
+            self.container4.pack()
 
-        self.label = tk.Label (text = "FAZER LOGIN", font = ("Arial", 25, "italic", "bold"), foreground = "black", background = "cyan", width = 50, height =8)
+            self.container1 = Frame(master)
+            self.container1["pady"] = 10
+            self.container1.pack()       
 
-        self.label.pack()
 
- 
+            #adicionar informacoes
 
- 
+            self.nome = Entry(self.container2)
+            self.nome["width"] = 25
+            self.nome["font"] = "Verdana", 18, "italic"
+            self.nome.pack(side=LEFT)
 
-        self.container6 = Frame(master)
 
-        self.container6["padx"] = 20
+            self.nome = Entry(self.container4)
+            self.nome["width"] = 25
+            self.nome["font"] = "Verdana", 18, "italic"
+            self.nome.pack(side= LEFT)
+            
 
-        self.container6["pady"] = 5
+    root = Tk()
 
-        self.container6.pack()
 
- 
+    root.geometry('900x700')
+    root.configure(background="#62c9de")
+    Application(root)
 
- 
+    root.mainloop()
 
-        self.container1 = Frame(master)
 
-        self.container1["pady"] = 10
-
-        self.container1.pack()       
-
- 
-
-        self.lblnome = tk.Label (text = "Email:", font=("Verdana", 18, "italic"), foreground = "black", background = "cyan", width = 50, height =20)
-
- 
-
-        self.lblnome.pack(side=LEFT)
-
- 
-
-        self.lblnome = tk.Label (text = "Senha:", font=("Verdana", 18, "italic"), foreground = "black", background = "cyan", width = 50, height =40)
-
- 
-
-        self.lblnome.pack(side=LEFT)
-
-       
-
-root = Tk()
-
- 
-
-root.geometry('900x700')
-root.configure(background='cyan')
-Application(root)
-
-root.mainloop()
+'''
+            self.lblnome = tk.Label (text = "Email:", font=("Verdana", 18, "italic"), foreground = "black", background = "#62c9de", width = 30, height =10)
+            self.lblnome.pack(side=LEFT)
+            self.lblnome = tk.Label (text = "Senha:", font=("Verdana", 18, "italic"), foreground = "black", background = "#62c9de", width = 30, height =40) 
+            self.lblnome.pack(side=LEFT)
+            
+'''

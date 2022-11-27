bee3f0fa3945c70881ac894bad0f46909ba73682
clara2023 <105126932+clara2023@users.noreply.github.com>
2022-11-27 18:12:52 -0300

    Update visualizador_login.py

--- visualizador_login.py
+++ visualizador_login.py
@@ -3,57 +3,109 @@ import tkinter as tk
 
 class Application:
 
-    def __init__ (self, master = None):
+    def __init__ (self, master):
+        for widget in master.winfo_children():
+            widget.destroy()    
         
-        self.fonte = ("Verdana", "8")
-        self.label = tk.Label (text = "FAZER LOGIN", font = ("Arial", 25, "italic", "bold"), foreground = "black", background = "#62c9de", width = 50, height =8)
+        self.label = tk.Label (text = "Menu", font = ("Calibri", 25, "bold"), foreground = "black", width = 50, height =8)
         self.label.pack()
 
-        self.container3 = Frame(master)
-        self.container3["padx"] = 180
-        self.container3["pady"] = 60
-        self.container3.pack()
+        self.label_email = Label (master, text = 'Email:', font = ('Bahnschrift Light SemiCondensed', 15,'bold'))
+        self.label_email.pack()
 
-        self.container6 = Frame(master)
-        self.container6["padx"] = 20
-        self.container6["pady"] = 5
-        self.container6.pack()
-
-        self.container2 = Frame(master)
-        self.container2["padx"] = 20
-        self.container2.pack()
+        self.nome = Entry(master, width = 25, font = ("Verdana", 18, "italic"))
+        self.nome.pack()
         
-        self.container4 = Frame(master)
-        self.container4["padx"] = 20
-        self.container4["pady"] = 50
-        self.container4.pack()
-
-        self.container1 = Frame(master)
-        self.container1["pady"] = 10
-        self.container1.pack()       
-
-        self.lblnome = tk.Label (text = "Email:", font=("Verdana", 18, "italic"), foreground = "black", background = "#62c9de", width = 80)
-        self.lblnome.pack(side=LEFT)
-        #adicionar informacoes
-
-        self.nome = Entry(self.container2)
-        self.nome["width"] = 25
-        self.nome["font"] = "Verdana", 18, "italic"
-        self.nome.pack(side=LEFT)
-
-
-        self.nome = Entry(self.container4)
-        self.nome["width"] = 25
-        self.nome["font"] = "Verdana", 18, "italic"
-        self.nome.pack(side= LEFT)
+        self.label_senha = Label (master, text = 'Senha:', font = ('Bahnschrift Light SemiCondensed', 15,'bold'))
+        self.label_senha.pack()
         
+        self.senha = Entry(master, width = 25, font = ("Verdana", 18, "italic"))
+        self.senha.pack()
+        #self.senha["show"] = "*"
+
+
+        self.entrar = Button(master, text = 'Entrar', font = ('Calibri','12'),width = 20, command = lambda: self.verificaSenha(master, self.nome.get(),self.senha.get()))
+        self.entrar.pack()
+
+        self.entrar = Button(master, text = 'Cadastrar-se', font = ('Calibri','12'),width = 20, command = lambda : self.cadastro(master))
+        self.entrar.pack()
+
+
+    def cadastro(self,master):
+        for widget in master.winfo_children():
+            widget.destroy()
+            
+        
+        self.label = tk.Label (text = "Cadastro", font = ("Calibri", 25, "bold"), foreground = "black", width = 50, height =8)
+        self.label.pack()
+
+        self.label_email = Label (master, text = 'Email:', font = ('Bahnschrift Light SemiCondensed', 15,'bold'))
+        self.label_email.pack()
+
+        self.nome = Entry(master, width = 25, font = ("Verdana", 18, "italic"))
+        self.nome.pack()
+        
+        self.label_senha = Label (master, text = 'Senha:', font = ('Bahnschrift Light SemiCondensed', 15,'bold'))
+        self.label_senha.pack()
+        
+        self.senha = Entry(master, width = 25, font = ("Verdana", 18, "italic"))
+        self.senha.pack()
+        
+        self.label_senha = Label (master, text = 'Confirma senha:', font = ('Bahnschrift Light SemiCondensed', 15,'bold'))
+        self.label_senha.pack()              
+            
+        self.confirma_senha= Entry(master, width = 25, font = ("Verdana", 18, "italic"))
+        self.confirma_senha.pack()
+
+
+        self.entrar = Button(master, text = 'Cadastrar', font = ('Calibri','12'),width = 20, command = lambda : self.faz_cadastro(master, self.nome.get(),self.senha.get(),self.confirma_senha.get()))
+        self.entrar.pack()
+        
+        self.voltar = Button(master, text = 'Voltar', font = ('Calibri','12'),width = 20, command = lambda : self.__init__(master))
+        self.voltar.pack()    
+        
+    def faz_cadastro(self,master, nome, senha, confirma):
+        self.cadastro(master)
+
+        if senha == confirma:
+            file = open("teste.txt","a")
+            file.write(str(nome + ' ' + senha))
+            file.write('\n')
+            file.close()
+                       
+            self.confirmar = Label (master, text= 'Cadastro concluído com sucesso!', font =('Bahnschrift Light SemiCondensed', 15,'bold'),fg='green')
+            self.confirmar.pack()
+            master.after(2000, self.confirmar.destroy)
+            
+            
+        else:
+            
+            self.confirmar = Label (master, text= 'As senhas devem ser iguais!', font =('Bahnschrift Light SemiCondensed', 15,'bold'),fg='red')
+            self.confirmar.pack()
+                        
+
+    def verificaSenha(self,master,nome,senha):
+        
+        file = open("teste.txt","r")
+        texto = file.readlines()
+        flag = False
+        for linha1 in texto:
+            if str(nome + ' ' + senha) in linha1:
+                print('achei')
+                self.confirmar = Label (master, text= 'Usuário autenticado!', font =('Bahnschrift Light SemiCondensed', 15,'bold'),fg='green')
+                self.confirmar.pack()
+                flag = True
+                break
+        if flag == False:
+            print('nao achei')
+            self.confirmar = Label (master, text= 'Erro de autenticação!', font =('Bahnschrift Light SemiCondensed', 15,'bold'),fg='green')
+            self.confirmar.pack()
+
+        file.close()            
+            
 
 root = Tk()
-
-
 root.geometry('900x700')
-root.configure(background="#62c9de")
 Application(root)
-
 root.mainloop()
 

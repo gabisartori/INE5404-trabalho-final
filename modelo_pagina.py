ad74590b152c029a6602b1ae0613af96ccb22d83
Gabriel Sartori Rangel <gabriel.sartorirangel@gmail.com>
2022-11-28 18:05:37 -0300

    erro brutal

--- modelo_pagina.py
+++ modelo_pagina.py
@@ -6,7 +6,7 @@ class Pagina:
 
 
 class PaginaDiario(Pagina):
-    def __init__(self, numero: int, livro: str, texto: str or str[list]) -> None:
+    def __init__(self, numero: int, livro: str, texto: str or list[str]) -> None:
         super().__init__(numero, livro, texto)
         while len(self.texto) < 10:
             self.texto.append('')

9e39240134f3872294b818dc807d9974b2132f6b
Gabriel Sartori Rangel <gabriel.sartorirangel@gmail.com>
2022-11-29 01:40:05 -0300

    arquivo main sendo o principal dnv

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-from visualizador_livro import VisualizadorLivro
-from visualizador_diario import VisualizadorDiario
+from visualizador_login import VisualizadorLogin
 
-# livro = input("Digite o nome do livro: ")
-livro = "coisa"
-
-# a = int(input('livro ou diario? [1/2] '))
-
-a = 2
-
-if a == 1:
-  visualizador = VisualizadorLivro(livro)
-else:
-  visualizador = VisualizadorDiario('gabriel')
-
-visualizador.run()
+tela = VisualizadorLogin()
+tela.run()
+tela.root.mainloop()

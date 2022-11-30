991466e47273de143e7c77ff25fe993c3e04f555
Gabriel Sartori Rangel <gabriel.sartorirangel@gmail.com>
2022-11-30 18:26:33 -0300

    Update modelo_pagina.py

--- modelo_pagina.py
+++ modelo_pagina.py
@@ -8,5 +8,7 @@ class Pagina:
 class PaginaDiario(Pagina):
     def __init__(self, numero: int, livro: str, texto: str or list[str]) -> None:
         super().__init__(numero, livro, texto)
+        if isinstance(texto, str): self.texto = texto.split('\n')
+
         while len(self.texto) < 10:
             self.texto.append('')
